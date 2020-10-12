ac2f7d820541aa5b4292a452f38d948015338313
Michael Manurung <michaelzeros8@gmail.com>
2020-10-12 13:51:10 +0700

    Create MenuFragment.java

---
+++ app/src/main/java/com/example/tugasbesar01/MenuFragment.java
@@ -0,0 +1,41 @@
+package com.example.tugasbesar01;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.zip.Inflater;
+
+public class MenuFragment extends Fragment {
+
+    public static MenuFragment newInstance(String title) {
+        MenuFragment fragment = new MenuFragment();
+        Bundle args = new Bundle();
+        args.putString("title",title);
+        fragment.setArguments(args);
+        return fragment;
+
+
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu_fragment,container,false);
+
+        FloatingActionButton fab = view.findViewById(R.id.fabPlus);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
+    }
+}

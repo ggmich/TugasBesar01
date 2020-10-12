c19df6b39f2bb67ce6dfb818113467f144e3c96f
Michael Manurung <michaelzeros8@gmail.com>
2020-10-12 11:40:32 +0700

    mainActivity

--- app/src/main/java/com/example/tugasbesar01/MainActivity.java
+++ app/src/main/java/com/example/tugasbesar01/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.tugasbesar01;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    FragmentManager fragmentManager;
+    FragmentTransaction ft;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        fragmentManager = this.getSupportFragmentManager();
+        this.toolbar = findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TOMBOL GARIS 3
+        ActionBarDrawerToggle adbt = new ActionBarDrawerToggle(this,drawer,R.string.openDrawer,R.string.closeDrawer);
+        drawer.addDrawerListener(adbt);
+        adbt.syncState();
     }
 }

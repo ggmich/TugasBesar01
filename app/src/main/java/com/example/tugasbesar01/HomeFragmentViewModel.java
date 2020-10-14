80b64988afcd9e2a05c8bdac0ae6555704da1f12
Michael Manurung <michaelzeros8@gmail.com>
2020-10-14 14:50:54 +0700

    Update HomeFragmentViewModel.java

--- app/src/main/java/com/example/tugasbesar01/HomeFragmentViewModel.java
+++ app/src/main/java/com/example/tugasbesar01/HomeFragmentViewModel.java
@@ -1,12 +1,15 @@
 package com.example.tugasbesar01;
 
 import android.content.ClipData;
+import android.content.SharedPreferences;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class HomeFragmentViewModel extends ViewModel {
 
@@ -30,11 +33,27 @@ public class HomeFragmentViewModel extends ViewModel {
 
 
     public LinkedList<String> getList() {
+
+
         return list;
     }
 
     public void setList(LinkedList<String> list) {
         this.list = list;
+    }
+
+    public int randomMenuTitle(String list[]){
+        Random rn = new Random();
+        int res = 0;
+
+        int max = list.length - 1;
+
+        res = rn.nextInt(max - 0 + 1) + 0;
+
+
+        //System.out.println(Arrays.toString(res));
+
+        return res;
     }
 
 
